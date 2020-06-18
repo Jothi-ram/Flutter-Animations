a0f1becfa67975d329ea6bb935f2d1ed7362da85
Jothi-Ram <ramukuttytheiva@gmail.com>
2020-06-18 21:14:06 +0530

    Right and Left Flpas Added

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/cat.dart';
+import 'dart:math';
 
 class Home extends StatefulWidget{
   HomeState createState() => HomeState();
@@ -8,16 +9,40 @@ class Home extends StatefulWidget{
 class HomeState extends State<Home> with TickerProviderStateMixin{
    Animation<double>catAnimation;
    AnimationController catController;
+   Animation<double> boxAnimation;
+   AnimationController boxController;
 
    initState(){
      super.initState();
 
+    boxController = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this, 
+      );
+    boxAnimation = Tween(
+      begin: pi * 0.6,
+      end: pi * 0.65,
+      ).animate(
+      CurvedAnimation(
+        parent: boxController, 
+        curve: Curves.linear,
+      ),
+    );
+    boxAnimation.addStatusListener((status) {
+      if(status == AnimationStatus.completed){
+        boxController.reverse();
+      }else if (status == AnimationStatus.dismissed){
+        boxController.forward();
+      }
+    });
+    boxController.forward();
+
      catController = AnimationController(
-       duration: Duration(seconds: 2),
+       duration: Duration(milliseconds: 200),
        vsync: this,
      );
 
-     catAnimation = Tween(begin: 0.0, end: 100.0)
+     catAnimation = Tween(begin: -35.0, end: -80.0)
      .animate(
        CurvedAnimation(parent: catController,
         curve: Curves.easeIn,
@@ -25,26 +50,99 @@ class HomeState extends State<Home> with TickerProviderStateMixin{
      );
    }
 
+  onTap(){
+      if(catController.status == AnimationStatus.completed){
+        catController.reverse();
+      }else if(catController.status == AnimationStatus.dismissed){
+        catController.forward();
+      }
+  }
+
 
   Widget build(context){
     return Scaffold(
     appBar: AppBar(
       title: Text('Animation!'),
     ),
-    body: buildAnimation(),
-  );
+    body: GestureDetector(
+      child: Center(
+        child:  Stack(
+          overflow: Overflow.visible,
+        children: [
+          buildCatAnimation(),
+          buildBox(),
+          buildLeftFlap(),
+          buildRightFlap(),
+        ],
+      ),
+    ),
+      onTap: onTap,
+    ),
+   );
   }
 
-  Widget buildAnimation(){
+  Widget buildCatAnimation(){
     return AnimatedBuilder(
       animation: catAnimation,
       builder: (context,child){
-        return Container(
+        return Positioned(
           child: child,
-          margin: EdgeInsets.only(top: catAnimation.value),
+          top: catAnimation.value,
+          right: 0.0,
+          left: 0.0,
         );
       },
       child: Cat(),
     );
+  }
+
+  Widget buildBox(){
+    return Container(
+      height: 200.0,
+      width: 200.0,
+      color: Colors.brown,
+    );
+  }
+
+  Widget buildLeftFlap(){
+    return Positioned(
+      left: 3.0,
+      child: AnimatedBuilder(
+      animation: boxAnimation,
+      child: Container(
+          height:10.0,
+          width: 125.0,
+          color: Colors.brown,
+        ),
+    builder: (context, child){
+      return Transform.rotate(
+        child:child,
+        alignment: Alignment.topLeft,
+        angle: boxAnimation.value,
+       );
+     },
+    ),
+   );
+  }
+
+  Widget buildRightFlap(){
+    return Positioned(
+      right: 3.0,
+      child: AnimatedBuilder(
+      animation: boxAnimation,
+      child: Container(
+          height:10.0,
+          width: 125.0,
+          color: Colors.brown,
+        ),
+    builder: (context, child){
+      return Transform.rotate(
+        child:child,
+        alignment: Alignment.topRight,
+        angle: -boxAnimation.value,
+       );
+     },
+    ),
+   );
   }
 }
--- lib/src/widgets/cat.dart
+++ lib/src/widgets/cat.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class Cat extends StatelessWidget{
   build(context){
     return Image.network(
+      //'https://i.imgur.com/A0zkKlT.jpg'
       'https://i.imgur.com/QwhZRyL.png'
     );
   }
